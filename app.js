ea3367f197d40dc6912017a07b0502c65d46f748
Unknown <encore.ivan@gmail.com>
2018-12-31 06:02:10 +0100

    Adjust code style

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-const request = require("request")
-const argv = require("yargs").argv
+const request = require('request')
+const argv = require('yargs').argv
 let userToken = argv.token
 let nocollect = !!argv.nocollect
 
 if (userToken === undefined || userToken.length !== 213)
-    throw Error("No valid token provided")
+    throw Error('No valid token provided')
 
 let getActivePoints = () => {
     const options = {
-        url: "https://api.nasapatrola.com/Points/GetActivePoints",
+        url: 'https://api.nasapatrola.com/Points/GetActivePoints',
         headers: { Authorization: userToken }
-    };
-    return new Promise(function(resolve, reject) {
-        request.get(options, function(err, resp, body) {
+    }
+    return new Promise((resolve, reject) => {
+        request.get(options, (err, resp, body) => {
             if (err) reject(err)
             else resolve(body)
-        });
-    });
-};
+        })
+    })
+}
 
 let collectPoints = id => {
     const options = {
-        url: "https://api.nasapatrola.com/Points/CollectPoint",
+        url: 'https://api.nasapatrola.com/Points/CollectPoint',
         headers: { Authorization: userToken },
         body: JSON.stringify({id})
-    };
+    }
     return new Promise((resolve, reject) => {
         request.post(options, (err, resp, body) => {
             if (err) reject(err)
             else resolve(body)
-        });
-    });
-};
+        })
+    })
+}
 
 const errHandler = (err) => console.error(err)
 
 let main = () => {
-    console.log(`Iteration ran at ${new Date().toUTCString()}`);
+    console.log(`Iteration ran at ${new Date().toUTCString()}`)
     getActivePoints()
         .then(JSON.parse, errHandler)
         .then(result => {
             console.log(`Found ${result.length} active points`, result);
 
-            if (nocollect) return;
+            if (nocollect) return
 
             result.map(({id}) => {
                 collectPoints(id)
@@ -51,11 +51,11 @@ let main = () => {
                     console.log(`Collected ${poins} points from active points id ${id}`)
                 })
                 .catch((err) => {
-                    console.error(`Unable to retrieve points for id ${id}\n${err}`)
-                });
+                    console.error(`Unable to retrieve points for id ${id}`, err)
+                })
             })
         }, errHandler)
 }
 
 main()
-setInterval(main, 1000 * 60 * 5);
+setInterval(main, 1000 * 60 * 5)

07fad2dcb1c9c3baf129a56928d113e840cb6190
Unknown <encore.ivan@gmail.com>
2019-01-02 01:46:56 +0100

    Handle token expiration error

--- app.js
+++ app.js
@@ -47,32 +47,36 @@ let getCurrentPoints = id => {
 }
 
 const errHandler = (err) => console.error(err)
+const tokenExpired = (body) => {
+    if(body.includes('expired'))
+        throw new Error('Provided token has expired. Token re-acquirement has not been implemented.')
+    return body
+}
 
 let main = () => {
     if (argv.id){
         getCurrentPoints(argv.id)
-            .then(JSON.parse, errHandler)
-            .then(({ username, id, points }) => console.log(`User ${username} (${id}) has ${points} points`), errHandler)
-            .catch(errHandler);
-        return;
+            .then(body => tokenExpired(body))
+            .then(JSON.parse)
+            .then(({ username, id, points }) => console.log(`User ${username} (${id}) has ${points} points`))
+            .catch(errHandler)
+        return
     }
     console.log(`Iteration ran at ${new Date().toUTCString()}`)
     getActivePoints()
+        .then(body => tokenExpired(body))
         .then(JSON.parse, errHandler)
         .then(result => {
-            console.log(`Found ${result.length} active points`, result);
+            console.log(`Found ${result.length} active points`, result)
 
             if (nocollect) return
 
             result.map(({ id }) => {
                 collectPoints(id)
-                .then(JSON.parse, errHandler)
-                .then(({ poins }) => {
-                    console.log(`Collected ${poins} points from active points id ${id}`)
-                })
-                .catch((err) => {
-                    console.error(`Unable to retrieve points for id ${id}`, err)
-                })
+                    .then(body => tokenExpired(body))
+                    .then(JSON.parse, errHandler)
+                    .then(({ poins }) => console.log(`Collected ${poins} points from active points id ${id}`))
+                    .catch(err => console.error(`Unable to retrieve points for id ${id}`, err))
             })
         }, errHandler)
 }
